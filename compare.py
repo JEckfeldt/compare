bd36d5db6ede4b3fcf62cca281501467c2b03b41
Jake Eckfeldt <jaeckfeldt@outlook.com>
2024-04-28 13:51:35 -0700

    .

--- compare.py
+++ compare.py
@@ -8,22 +8,19 @@ def getFiles(dir, excludeSize, userAgent):
     i = 0
     for root, dirs, files in os.walk(dir):
         for file in files:
-            # print(file)
             filePath = os.path.join(root, file)
             # if file is valid, is not the normal size, and the userAgent is in filename
             if os.path.isfile(filePath) and os.path.getsize(filePath) != excludeSize and (userAgent in file):
                 if file not in matchingFiles:
                     matchingFiles.append(filePath)
-                    print(len(file))
-                    i = i + 1
     
     return matchingFiles
 
-dir = '/home/xu/f5/testsite/json'
+path = '/home/xu/f5/testsite/json'
 userAgent = 'Mozilla_5_0__Windows_NT_10_0__Win64__x64__rv_125_0__Gecko_20100101_Firefox_125_0_'
 size = 18
 
-files = getFiles(dir, size, userAgent)
-print("Files with size not equal to 18 and has useragent: ", len(files))
-# for file in files:
-#     print(file)
+files = getFiles(path, size, userAgent)
+print("Files with size not equal to 18 and has useragent: ")
+for file in files:
+    print(file)
