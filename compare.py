6fe47a6313b601a74f106855761b3ccb0f70b434
Jake Eckfeldt <jaeckfeldt@outlook.com>
2024-04-28 18:41:56 -0700

    .

--- compare.py
+++ compare.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # Return list of files not matching certain size
 # Also match the user agent
@@ -6,14 +7,18 @@ import os
 def getFiles(dir, excludeSize, userAgent):
     matchingFiles = []
     i = 0
-    for root, dirs, files in os.walk(dir):
-        for file in files:
-            filePath = os.path.join(root, file)
-            # if file is valid, is not the normal size, and the userAgent is in filename
-            if os.path.isfile(filePath) and os.path.getsize(filePath) != excludeSize and (userAgent in file):
-                if file not in matchingFiles:
-                    matchingFiles.append(filePath)
-    
+    try:
+        # Walk through the directory
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                filePath = os.path.join(root, file)
+                # if file is valid, is not the normal size, and the userAgent is in filename
+                if os.path.isfile(filePath):
+                    if os.path.getsize(filePath) != excludeSize and (userAgent in file):
+                        if file not in matchingFiles:
+                            matchingFiles.append(filePath)
+    except Exception as e:
+        print(f"Error: {e}")
     return matchingFiles
 
 path = '/home/xu/f5/testsite/json'
@@ -21,7 +26,15 @@ userAgent = 'Mozilla_5_0__Windows_NT_10_0__Win64__x64__rv_125_0__Gecko_20100101_
 size = 18
 # get files with user agent that are different from the base
 files = getFiles(path, size, userAgent)
+print("Found " + len(files) + " files\n")
 
-print("Files with size not equal to 18 and has useragent: ", len(files))
+# Get the base file to make comparisons
 base = next((file for file in files if 'base' in file), None)
 print('Base file: ', base)
+
+# load the base json data and get the original fonts
+with open(base) as jsonFile:
+    data = json.load(jsonFile):
+
+originalFonts = data['components']['fonts']['value']
+print(originalFonts) 
