ecba0c1ad9f487a342c0ed18109277aae7d059b9
Jake Eckfeldt <jaeckfeldt@outlook.com>
2024-05-14 01:39:39 -0700

    .

--- compare.py
+++ compare.py
@@ -113,7 +113,7 @@ def getChangedAttributes(files):
     for file in files:
         # print(file)
         if prevFileSize is not None and os.path.getsize(file) != prevFileSize:
-            # print(file)
+            print(file)
             # get the unstable attribute for the file and create a changes object
             if not os.path.exists(file):
                 print("File not found")
@@ -196,7 +196,7 @@ def countUnstable(files):
             if value in unstable:
                 unstable[value] += 1
             else:
-                # print(file)
+                print(file)
                 unstable[value] = 1
 
     return unstable
