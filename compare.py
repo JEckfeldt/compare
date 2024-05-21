d6661244175a0a75d05458ab244ce13941d4c8c1
Jake Eckfeldt <jaeckfeldt@outlook.com>
2024-05-21 11:56:57 -0700

    .

--- compare.py
+++ compare.py
@@ -194,6 +194,7 @@ def getFonts(files):
                         uniqueNew.add(fontData["new"])
                         newFonts[fontData["new"]] = newFonts.get(fontData["new"], 0) + 1
                 uniqueChanged = uniqueNew ^ uniqueOriginal
+                print(uniqueChanged)
                 for font in uniqueChanged:
                     if font in testFonts:
                         results[font] = results.get(font, 0) + 1 
