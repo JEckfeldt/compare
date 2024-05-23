325e591fedeff975aca8d4b210e4c6a3c689a463
Jake Eckfeldt <jaeckfeldt@outlook.com>
2024-05-22 23:01:24 -0700

    .

--- compare.py
+++ compare.py
@@ -50,7 +50,7 @@ size = 18 # file size to exclude
 
 # Windows
 # Firefox
-# userAgent = 'Mozilla_5_0__Windows_NT_10_0__Win64__x64__rv_125_0__Gecko_20100101_Firefox_125_0_'
+userAgent = 'Mozilla_5_0__Windows_NT_10_0__Win64__x64__rv_125_0__Gecko_20100101_Firefox_125_0_'
 # Edge
 # userAgent = 'Mozilla_5_0__Windows_NT_10_0__Win64__x64__AppleWebKit_537_36__KHTML__like_Gecko__Chrome_124_0_0_0_Safari_537_36_Edg_124_0_0_0_'
 # Chrome
@@ -63,7 +63,7 @@ size = 18 # file size to exclude
 # Chrome
 # userAgent = 'Mozilla_5_0__Macintosh__Intel_Mac_OS_X_10_15_7__AppleWebKit_537_36__KHTML__like_Gecko__Chrome_124_0_0_0_Safari_537_36_'
 # Firefox
-userAgent = 'Mozilla_5_0__Macintosh__Intel_Mac_OS_X_10_15__rv_125_0__Gecko_20100101_Firefox_125_0_'
+# userAgent = 'Mozilla_5_0__Macintosh__Intel_Mac_OS_X_10_15__rv_125_0__Gecko_20100101_Firefox_125_0_'
 
 # iPhone
 # Safari
@@ -228,7 +228,7 @@ def findNumChanges():
     print("Files sorted: ", len(sortedFiles))
     print("Number of changes: ", len(changedFiles))
     print("Changes for Attributes: ", getChangedAttributes(sortedFiles))
-    print("Changes for fonts: ", len(getFonts(sortedFiles)))
+    # print("Changes for fonts: ", len(getFonts(sortedFiles)))
 
 findNumChanges()
 
