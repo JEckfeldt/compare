78774946391bee520c4d59ef7275fde9f5c950ed
Jake Eckfeldt <jaeckfeldt@outlook.com>
2024-05-23 01:52:58 -0700

    .

--- compare.py
+++ compare.py
@@ -50,7 +50,7 @@ size = 18 # file size to exclude
 
 # Windows
 # Firefox
-# userAgent = 'Mozilla_5_0__Windows_NT_10_0__Win64__x64__rv_125_0__Gecko_20100101_Firefox_125_0_'
+userAgent = 'Mozilla_5_0__Windows_NT_10_0__Win64__x64__rv_125_0__Gecko_20100101_Firefox_125_0_'
 # Edge
 # userAgent = 'Mozilla_5_0__Windows_NT_10_0__Win64__x64__AppleWebKit_537_36__KHTML__like_Gecko__Chrome_124_0_0_0_Safari_537_36_Edg_124_0_0_0_'
 # Chrome
@@ -82,7 +82,7 @@ size = 18 # file size to exclude
 
 # Linux
 # Chrome
-userAgent = 'Mozilla_5_0__X11__Linux_x86_64__AppleWebKit_537_36__KHTML__like_Gecko__Chrome_124_0_0_0_Safari_537_36_'
+# userAgent = 'Mozilla_5_0__X11__Linux_x86_64__AppleWebKit_537_36__KHTML__like_Gecko__Chrome_124_0_0_0_Safari_537_36_'
 # Firefox
 # userAgent = 'Mozilla_5_0__X11__Ubuntu__Linux_x86_64__rv_125_0__Gecko_20100101_Firefox_125_0_'
 
@@ -234,7 +234,7 @@ def getFonts(files):
             print(file)
             print(f"Error: {e}")
     sorted_dict = dict(sorted(results.items(), key=lambda item: item[1], reverse=True))
-    return uniqueFontLists
+    return sorted_dict
 
 def getUniqueValues(files):
     uniques = {}
